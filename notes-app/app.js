487c121018ccf983b1fa3cf5fca4387c6b03bcee
williamantoniozup <william.antonio@zup.com.br>
2020-09-24 00:10:27 -0300

    getting input from users

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,21 +1,34 @@
 const validator = require('validator')
 const chalk = require('chalk');
-const add = require('./utils')
-const getNotes = require('./notes')
+// const add = require('./utils')
+// const getNotes = require('./notes')
+//
+// console.log(add(1,1))
+//
+// const resultGetNotes = getNotes()
+// const success = chalk.keyword('green')
+// const error = chalk.bold.red
+//
+// console.log(resultGetNotes)
+//
+// console.log(validator.isEmail('will@gmail.com'))
+// console.log(validator.isURL('https://mead.io'))
+// console.log(success('Success!'))
+// console.log(error('Error!'))
+// console.log(chalk.blue.inverse.bgRed.bold('Hello World!'))
 
-console.log(add(1,1))
+//Getting input from users
+console.log(process.argv)
+// console.log(process.argv[2])
 
-const resultGetNotes = getNotes()
-const success = chalk.keyword('green')
-const error = chalk.bold.red
+const command = process.argv[2]
 
-console.log(resultGetNotes)
+if (command === 'add') {
+  console.log('Adding note!')
+} else if (command === 'remove') {
+  console.log('Removing note!')
+}
 
-console.log(validator.isEmail('will@gmail.com'))
-console.log(validator.isURL('https://mead.io'))
-console.log(success('Success!'))
-console.log(error('Error!'))
-console.log(chalk.blue.inverse.bgRed.bold('Hello World!'))
 /*
 cada modulo js possui seu proprio escopo
 
